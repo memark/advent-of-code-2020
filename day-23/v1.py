6e8b7cabacccc50c3352c89589d2c219ac2ad323
Magnus Markling <magnus@markling.com>
2020-12-24 07:42:24 +0100

    wip opt

--- day-23/v1.py
+++ day-23/v1.py
@@ -1,6 +1,8 @@
 # pyright: reportWildcardImportFromLibrary=false
 # pyright: reportMissingImports=false, reportUndefinedVariable=false
 
+from bisect import *
+import cProfile
 from collections import *
 import functools
 import operator
@@ -35,7 +37,8 @@ def getAnswer_2(data: str) -> int:
 
 def getAnswer(data: str, *, isPartTwo: bool) -> int:
     def pretty_cups():
-        new_cups = [*cups[0:3], cups[-3]] if isPartTwo else cups
+        n = 8
+        new_cups = [*cups[:n], '  -  ', cups[-n:]] if isPartTwo else cups
         # Skriv om som list comprehension!
         s = list()
         for c in new_cups:
@@ -51,12 +54,11 @@ def getAnswer(data: str, *, isPartTwo: bool) -> int:
     # the cups would be labeled 3, 2, 4, 1, 5, and then back to 3 again.
 
     cups = [int(d) for d in data]
-    L = len(cups)
     if isPartTwo:
         cups.extend(range(max(cups)+1, 1000000+1))
     lowest_cup_label = min(cups)
     highest_cup_label = max(cups)
-    print(lowest_cup_label, highest_cup_label)
+    print(lowest_cup_label, '-', highest_cup_label)
 
     # Before the crab starts, it will designate the first cup in your list as the current cup.
     curr_label = cups[0]
@@ -65,22 +67,54 @@ def getAnswer(data: str, *, isPartTwo: bool) -> int:
     # Part Two: After discovering where you made the mistake in translating Crab Numbers,
     # you realize the small crab isn't going to do merely 100 moves;
     # the crab is going to do ten million (10000000) moves!
-    num_moves = 100 if isPartTwo else 100
+    num_moves = 10  # 100 if isPartTwo else 100
+
+    L = len(cups)
+
+    cache: dict[int, int] = dict()
+
+    def cachedIndex(item: int) -> int:
+        if item in cache and cups[cache[item]] == item:
+            return cache[item]
+
+        if item in cache:
+            index = cache[item]
+            for i in range(-4, 4+1):
+                temp = cups[index+i]
+                if temp == item:
+                    cache[item] = temp
+                    return temp
+
+        res = cups.index(item)
+        cache[item] = res
+        return res
 
     # Each move, the crab does the following actions:
     for m in range(1, num_moves + 1):
-        # print(f'-- move {m} --')
-        # print(f'cups: {pretty_cups()}')
+        print(f'-- move {m} --')
+        print(f'cups: {pretty_cups()}')
 
         # The crab picks up the three cups
         # that are immediately clockwise of the current cup.
         # They are removed from the circle;
         # cup spacing is adjusted as necessary to maintain the circle.
-        picked_up = [cups.pop((cups.index(curr_label)+1) % len(cups)),
-                     cups.pop((cups.index(curr_label)+1) % len(cups)),
-                     cups.pop((cups.index(curr_label)+1) % len(cups))]
-        # print(f'pick up: {picked_up}')
-        # print(f'cups: {pretty_cups()}')
+
+        picked_up = list()
+        curr_index = cachedIndex(curr_label)
+        for i in range(3):
+            pop_index = (curr_index+1) % L
+            picked_up.append(cups.pop(pop_index))
+            L -= 1
+            if pop_index < curr_index:
+                curr_index -= 1
+                # print('adjusted curr_index')
+
+        # picked_up = [cups.pop((cups.index(curr_label)+1) % len(cups)),
+        #              cups.pop((cups.index(curr_label)+1) % len(cups)),
+        #              cups.pop((cups.index(curr_label)+1) % len(cups))]
+
+        print(f'pick up: {picked_up}')
+        print(f'cups: {pretty_cups()}')
 
         # The crab selects a destination cup:
         # the cup with a label equal to the current cup's label minus one.
@@ -94,7 +128,8 @@ def getAnswer(data: str, *, isPartTwo: bool) -> int:
             dest_label -= 1
             if dest_label < lowest_cup_label:
                 dest_label = highest_cup_label
-        dest_index = cups.index(dest_label)
+        # dest_index = cups.index(dest_label)
+        dest_index = cachedIndex(dest_label)
         # print(f'destination: {dest_label}')
 
         # The crab places the cups it just picked up
@@ -102,11 +137,13 @@ def getAnswer(data: str, *, isPartTwo: bool) -> int:
         # They keep the same order as when they were picked up.
         # Att använda cups.insert() gav ingen skillnad i hastighet.
         cups[dest_index+1:dest_index+1] = picked_up
+        L += 3
         # print(f'cups: {pretty_cups()}')
 
         # The crab selects a new current cup:
         # the cup which is immediately clockwise of the current cup.
-        new_index = (cups.index(curr_label)+1) % len(cups)
+        # new_index = (cups.index(curr_label)+1) % L
+        new_index = (cachedIndex(curr_label)+1) % L
         curr_label = cups[new_index]
 
         # print()
@@ -115,27 +152,33 @@ def getAnswer(data: str, *, isPartTwo: bool) -> int:
     # print(f'cups: {pretty_cups()}')
     # print()
 
-    if not isPartTwo:
-        index1 = cups.index(1)
-        s = ''
-        for i in range(index1+1, index1+len(cups)):
-            s = s + str(cups[i % len(cups)])
-        # print(s)
-        return int(s)
-    else:
-        index1 = cups.index(1)
-        label_star_1 = int(cups[(index1+1) % len(cups)])
-        label_star_2 = int(cups[(index1+2) % len(cups)])
-        # print(label_star_1)
-        # print(label_star_2)
-        return label_star_1 * label_star_2
+    def score():
+        if not isPartTwo:
+            index1 = cups.index(1)
+            s = ''
+            for i in range(index1+1, index1+len(cups)):
+                s = s + str(cups[i % len(cups)])
+            # print(s)
+            return int(s)
+        else:
+            index1 = cups.index(1)
+            label_star_1 = int(cups[(index1+1) % len(cups)])
+            label_star_2 = int(cups[(index1+2) % len(cups)])
+            # print(label_star_1)
+            # print(label_star_2)
+            return label_star_1 * label_star_2
+
+    return score()
 
 
 def main() -> None:
-    # getAndPrintAndAssertAndTimeAnswer(getAnswer_1, sample_1, 67384529)
-    getAndPrintAndAssertAndTimeAnswer(getAnswer_1, input, 27865934)
+    # getAndPrintAndAssertAndTimeAnswer(        getAnswer_1, sample_1, 92658374) # För 10
+    # getAndPrintAndAssertAndTimeAnswer(
+    #     getAnswer_1, sample_1, 67384529)  # För 100
+    # getAndPrintAndAssertAndTimeAnswer(getAnswer_1, input, 64893572)  # För 1000
 
-    getAndPrintAndAssertAndTimeAnswer(getAnswer_2, sample_1, 12)  # För 100
+    # getAndPrintAndAssertAndTimeAnswer(getAnswer_2, sample_1, 12)  # För 100
+    getAndPrintAndAssertAndTimeAnswer(getAnswer_2, sample_1, 12)  # För 1000
     # getAndPrintAndAssertAndTimeAnswer(getAnswer_2, sample_2)
     # getAndPrintAndAssertAndTimeAnswer(getAnswer_2, input)
 
