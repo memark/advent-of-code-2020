f76ec27d45cdee8810fca3c3328e179ee8898b0b
Magnus Markling <magnus@markling.com>
2021-01-12 08:19:57 +0100

    wip

--- 2019/14/v1.hs
+++ 2019/14/v1.hs
@@ -1,6 +1,7 @@
 import qualified Data.Char as Char
 import qualified Data.List as List
 import qualified Data.Map.Strict as Map
+import Data.Map.Strict ((!), Map)
 import qualified Data.Set as Set
 import qualified Control.Exception as Ex
 import qualified Control.Monad
@@ -22,23 +23,31 @@ sample_1 = "\
 \6 A => 1 FUEL"
 
 -- \3 ORE => 1 B\n\
--- \3 A, 1 B => 1 FUEL"
+-- \3 A, 1 B => 1 FUEL"makeChem' :: [R] -> Map C Q -> C -> Q
 
-type Chemical = String
-type Quantity = Int
-type Input = (Quantity, Chemical)
-type Output = (Quantity, Chemical)
-type Reaction = ([Input], Output)
+type C = String
+type Q = Int
+type I = (Q, C)
+type O = (Q, C)
+type R = ([I], O)
+type T = Map Q C
+
+type RS = Map C (Q, Map C Q)
 
 main = do
   putStrLn ""
   input <- readFile "input.txt"
 
-  let d = parse sample_1a
-  putStrLn . List.intercalate "\n" . map show $ d
-  putStrLn ""
+  -- let d = parse sample_1a
+  -- putStrLn . List.intercalate "\n" . map show $ d
+  -- putStrLn ""
 
-  print $ makeChem d "FUEL"
+  -- print $ makeChem d "FUEL"
+  -- putStrLn ""
+
+  let d' = parse' sample_1a
+  print d'
+  putStrLn ""
 
   -- Part One -- 
 
@@ -46,21 +55,53 @@ main = do
 
   putStr ""
 
-makeChem d "ORE" = 1
-makeChem d chem = sum $ map (\ i -> fst i * makeChem d (snd i) `div` x) (fst r)
+
+
+makeChem' :: RS -> T -> C -> Q
+makeChem' _ _ "ORE" = 1
+makeChem' rs t c =
+  let
+    (q,is) = rs ! c
+    f = foldl ff ' is 
+    ff acc x = 
+  in
+    1
+
+
+parse' :: String -> Map C (Q, [(C, Q)])
+parse' = Map.fromList . map parseLine . lines . trim
   where 
-    x = fst (snd r)
-    r = findReaction d chem
+    parseLine :: String -> (C, (Q, [(C, Q)]))
+    parseLine = parseParts . toTuple . splitOn " => "
+
+    parseParts :: (String, String) -> (C, (Q, [(C, Q)]))
+    parseParts (is, o) = (fst po, (snd po, pis))
+      where pis = parseInputs is
+            po = parseNumChem o
+
+    parseInputs :: String -> [(C, Q)]
+    parseInputs a = map parseNumChem . splitOn ", " $ a
+
+    parseNumChem :: String -> (C, Q)
+    parseNumChem b = case splitOn " " b of [q,c] -> (c, read q :: Int)
 
 
-findReaction :: [Reaction] -> Chemical -> Reaction
-findReaction d oc =
+makeChem d "ORE" = 1
+makeChem d chem = sum $ map (\ i -> fst i * makeChem d (snd i) `div` oq) is
+  where 
+    (oq,oc) = o
+    (is,o) = r
+    r = findR d chem
+
+
+findR :: [R] -> C -> R
+findR d oc =
   case f d of Just c -> c
               Nothing -> error (show oc ++ " was not found")
     where f = List.find (\ (is,o) -> snd o == oc)
 
 
-parse :: String-> [([Input], Output)]
+parse :: String-> [([I], O)]
 parse = map parseLine . lines . trim
   where parseLine = parseParts . toTuple . splitOn " => "
         parseParts (inputs, output) = (parseInputs inputs, parseNumChem output)
